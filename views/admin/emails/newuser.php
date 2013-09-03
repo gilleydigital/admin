985919235b577408e6354e90b97f6e61150d70da
Chris Gilley <chris@gilleydigital.com>
2013-09-02 23:47:13 -0400

    Replaced DOMAIN with URL::base()

--- views/admin/emails/newuser.php
+++ views/admin/emails/newuser.php
@@ -1,3 +1,3 @@
-<p><a href="http://<?php echo DOMAIN.URL::base() ?>admin"><?php echo DOMAIN.URL::base() ?>admin</a></p>
+<p><a href="http://<?php echo URL::base('http') ?>admin"><?php echo URL::base('http') ?>admin</a></p>
 <p><b>Username:</b> <?php echo $username; ?></p>
 <p><b>Password:</b> <?php echo $password; ?></p>
