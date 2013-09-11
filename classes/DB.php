037d4bca9ae55ffa4cce91cc488caf746524b53f
Chris Gilley <chris@gilleydigital.com>
2013-09-11 00:57:21 -0400

    fixed some spacing in DB.php

--- classes/DB.php
+++ classes/DB.php
@@ -2,17 +2,17 @@
 
 class DB extends Kohana_DB {
 	//Check if tables exist
-	public static function check_for_tables ($tables)
+	public static function check_for_tables($tables)
 	{
 		if ( ! is_array($tables))
 		{
 			$tables = array($tables);
 		}
 		
-		foreach($tables AS $table)
+		foreach ($tables AS $table)
 		{
 			$result = DB::query(Database::SELECT, 'SHOW TABLES LIKE \''.$table.'\'')->execute();
-			if($result->count() === 0)
+			if ($result->count() === 0)
 			{
 				return false;
 			}
